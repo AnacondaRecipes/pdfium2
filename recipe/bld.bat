941b5e4b5d8805f2e6d897c115abe05b5d783e1e
Jack Olivieri <jolivieri@anaconda.com>
2023-08-21 20:11:35 +0200

    add bld bat script

---
+++ recipe/bld.bat
@@ -0,0 +1,10 @@
+@echo ON
+
+rem Copy over the include and lib dirs
+robocopy %SRC_DIR%\include\ %LIBRARY_INC%\ *.* /E
+if %ERRORLEVEL% GEQ 8 exit 1
+
+robocopy %SRC_DIR%\lib\ %LIBRARY_LIB%\ *.* /E
+if %ERRORLEVEL% GEQ 8 exit 1
+
+exit 0
