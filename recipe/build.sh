b133d3b4e6bfefef13dfdad982c2c031c7be7db8
Jack Olivieri <jolivieri@anaconda.com>
2023-08-21 20:11:22 +0200

    fix build script

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,13 +1,8 @@
-#!/bin/bash
-
-# Copy over the bin, include and lib dirs
-cp ${SRC_DIR}/bin/* ${LIBRARY_BIN}/
+# Copy over the include and lib dirs
+mkdir ${PREFIX}/include && cp -r ${SRC_DIR}/include/* ${PREFIX}/include
 if [[ $? -ne 0 ]]; then exit 1; fi
 
-cp ${SRC_DIR}/include/* ${LIBRARY_INC}/
-if [[ $? -ne 0 ]]; then exit 1; fi
-
-cp ${SRC_DIR}/lib/* ${LIBRARY_LIB}/
+mkdir ${PREFIX}/lib && cp -r ${SRC_DIR}/lib/* ${PREFIX}/lib
 if [[ $? -ne 0 ]]; then exit 1; fi
 
 # Add the licences to the recipe directory
