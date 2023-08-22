f707c5fb583e40ded11f6fd6e81c75fff467fe64
Jack Olivieri <jolivieri@anaconda.com>
2023-08-22 13:11:24 +0200

    fix build script

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,8 +1,8 @@
 # Copy over the include and lib dirs
-cp -r ${SRC_DIR}/include/* ${PREFIX}/include
+mkdir ${PREFIX}/include && cp -r ${SRC_DIR}/include/* ${PREFIX}/include
 if [[ $? -ne 0 ]]; then exit 1; fi
 
-cp -r ${SRC_DIR}/lib/* ${PREFIX}/lib
+mkdir ${PREFIX}/lib && cp -r ${SRC_DIR}/lib/* ${PREFIX}/lib
 if [[ $? -ne 0 ]]; then exit 1; fi
 
 exit 0
