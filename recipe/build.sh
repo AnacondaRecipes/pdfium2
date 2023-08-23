8d690f28137c6084ba1483f537c9924820e65c93
Jack Olivieri <jolivieri@anaconda.com>
2023-08-23 20:03:08 +0200

    install directories too

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,7 +2,8 @@
 for dir in "include" "include/cpp" "lib"
 do
     for filename in $(basename $(find ${SRC_DIR}/${dir} -type f -maxdepth 1) || exit 1)
-        do mkdir -p ${PREFIX}/${dir} && \
+    do
+        install -d ${PREFIX}/${dir} && \
         install -v -m644 ${SRC_DIR}/${dir}/${filename} ${PREFIX}/${dir}/${filename} || \
         exit 1
     done
